3af168876c320c7d62d59cd80b9bfcad11631ece
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2020-06-06 09:06:35 +0530

    added onclick function and setting edit textView

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun operationFunction(view: View){
+        var editText: EditText = findViewById(R.id.edit_text)
+
+        when(view.id){
+            R.id.button_seven -> editText.setText("7")
+            R.id.button_one -> editText.setText("1")
+        }
+
+
+
+    }
 }
+
+
